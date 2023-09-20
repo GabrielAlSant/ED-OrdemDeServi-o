98d231d214dd4a3e066fe8c8f7da3732d4d48a0b
Matheus_Andrade <msmatheus14@gmail.com>
2023-09-20 15:58:41 -0400

    Elaboração Menu principal em andamento 20.9 - 15h58

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print ("1 - Cadastrrar ordem de serviço")
+#Menu Principal - Caixa com menu dentro.
+
+def menu ():
+    
+    while true:
+    
+        print ("==========VRESTON -Ordens de Serviço==========")
+        print ("#                                            #")
+        print ("#        1 - Cadastrar Ordem se serviço      #")
+        print ("#        2 - Buscar Ordem de Serviço         #")
+        print ("#        3 - Listar Ordens de Serviço        #")
+        print ("#        4 - Atualizar Ordem de Serviço      #")
+        print ("#        5 - Excluir Ordem de Serviço        #")
+        print ("#        6 - Exibir média de custo           #")                  
+        print ("#        7 - Exibir OS com mair custo        #")                       
+        print ("#                                            #")
+        print ("#                                            #")
+        print ("#                                            #")
+        print ("#                                            #")
+        print ("##############################################")
+        
+menu ()
