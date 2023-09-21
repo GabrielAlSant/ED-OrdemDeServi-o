8bf6cc7de1383c3ee8d49c2aaa5e4030420fa7b1
Matheus_Andrade <msmatheus14@gmail.com>
2023-09-20 22:53:15 -0400

    Função Registrar Log e Listar Log funcionando

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 
 # Atributos: ID, Desc, Status, Solicitante, Custo, Equipamento, Executor.
 
-def menu (lista_OS, numeroOS):
+import datetime as dt
+
+def menu (lista_OS, lista_log, numeroOS):
     
     while True:
     
@@ -28,7 +30,7 @@ def menu (lista_OS, numeroOS):
         
         match opc:
             case 1:
-                cadastrar_OS(lista_OS,numeroOS)
+                cadastrar_OS(lista_OS,numeroOS, lista_log)
             case 2:
                 
                 print ("\n\n==========VRESTON -Ordens de Serviço==========")
@@ -36,10 +38,21 @@ def menu (lista_OS, numeroOS):
                 print ("#                                            #")
                 print ("#        1 - Buscar por Numero de OS         #")
                 print ("#        2 - Buscar por Descrição da OS      #")
+                print ("#        0 - Sair                            #")
                 print ("#                                            #")
                 print ("##############################################")
                 
                 opc = int(input("Digite uma opção>>>"))
+                
+                match opc:
+                    case 1:
+                        pass
+                    case 2:
+                        pass
+                    case 0:
+                        pass
+
+            
                 
             case 3:
                 pass
@@ -56,7 +69,9 @@ def menu (lista_OS, numeroOS):
             case 9:
                 pass
             case 10:
-                pass
+                
+                listarlog(lista_log)
+            
             case 0:
                 pass
             case _:
@@ -64,7 +79,7 @@ def menu (lista_OS, numeroOS):
 
 
 
-def cadastrar_OS(lista, numero_OS):
+def cadastrar_OS(lista, numero_OS, lista_log):
     
     pass
     
@@ -78,16 +93,33 @@ def cadastrar_OS(lista, numero_OS):
     equipamento = str(input("Digite o nome do Equipamento>>>"))
     executor = str(input("Digite o nome do executor da OS>>>"))
     
+    registrarLog ("Cadastro - ", lista_log)
+    
     OS = dict(numero = id, desc = desc, status = status, solicitante = solicitante, custo = custo, equipamento = equipamento, executor = executor )
+    
     
     lista_OS.append(OS)
     
+def registrarLog (tipo, lista_log):
     
+    tipo = tipo
+    horario = str (dt.datetime.now())
+    
+    registro = dict (tipo = tipo, horario = horario)
+    
+    lista_log.append(registro)
+    
+def listarlog (lista_log):
+    
+    for i in lista_log:
+        print (i)
+        
 
 
 lista_OS = []
+lista_log = []
 Numero_OS = int(0)
 
       
-menu (lista_OS, Numero_OS)
+menu (lista_OS, lista_log, Numero_OS)
 
